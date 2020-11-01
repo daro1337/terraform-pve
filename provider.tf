f45c509d4865cd93924c348b49d23fe424e9ce82
daro <adamczykdariusz1@gmail.com>
2020-11-01 19:52:32 +0100

    fix typo in readme

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "proxmox" {
     pm_api_url = "https://192.168.168.192:8006/api2/json"
     pm_user = "root@pam"
-    #pm_password  = password | or "export pm_password=pass"
+    #pm_password  = password | or "export PM_PASS=password"
     pm_tls_insecure = "true"
     pm_parallel = 8
     pm_timeout = 120
